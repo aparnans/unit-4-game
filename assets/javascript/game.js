46d28e0d03251a8a9c6eddced1c6897cab43e9a5
aparnans <aparna_ns@yahoo.com>
2019-07-02 13:20:38 -0400

    added wins losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,134 +11,83 @@ var lavenderNum;
 var redNum;
 var yellowNum;
 var greenNum;
+var winLoss = false;
 
 // start/reset function
 var startReset = function(){
     
-    totalRandomNum = Math.floor(Math.random()*99);
+    totalRandomNum = Math.floor(Math.random()*99) + 2;
     userTotal =0;
 
+    // reset wins ,losses only in case of start/reset
+    // and not when user wins or loses
+    if( !winLoss )
+    {
+        wins=0;
+        losses=0;
+    }
+    else
+    {
+        winLoss=false;
+    }
     lavenderNum= Math.floor(Math.random()*12) + 1;
     redNum= Math.floor(Math.random()*12) + 1;
     yellowNum= Math.floor(Math.random()*12) + 1;
     greenNum= Math.floor(Math.random()*12) + 1;
 
-    console.log("totalRandomNumber: ", totalRandomNum)
-    console.log(lavenderNum);
-    console.log(redNum);
-    console.log(totalRandomNum);
-    console.log(yellowNum);
     $("#totalNumber").text("Total Number : "+ totalRandomNum);
+    $("#userTotal").css("opacity","0.0");
     
     // assign attribute values to crystals
     $("#lavenderButton").attr("value", lavenderNum);
     $("#redButton").attr("value", redNum);
     $("#yellowButton").attr("value", yellowNum);
     $("#greenButton").attr("value", greenNum);
-
 }   
 
 startReset();
 
-$("start").on("click", function(){
+$("#start").on("click", function(){
     startReset();
 });
 
-console.log("Attribute Value " +$("#lavenderButton").attr("value"));
+var imgClick = function(idName){
 
-
- $("#lavenderButton").click(function(){
-    
-    num = $("#lavenderButton").attr("value");
+    var num = $(idName).attr("value");
     userTotal = userTotal + parseInt(num);    
-    
+
+    // Display the user total
+    $("#userTotal").css("opacity","1.0");
+    $("#userTotal").text("User Total: "+userTotal);
+
     if(userTotal===totalRandomNum) // this is a win
     {
         wins++;        
         $("#wins").text("Wins: "+wins);
+        winLoss=true;
         startReset();
     }
     else if(userTotal > totalRandomNum) // this is a loss
     {
         losses++;
-        $("#losses").text("Losses: "+losses);        
+        $("#losses").text("Losses: "+losses);
+        winLoss=true;        
         startReset();
     }
-    else
-    {
-        null;
-    }
-}); 
+}; 
 
 $("#redButton").click(function(){
-    
-    num = $("#redButton").attr("value");
-    userTotal = userTotal + parseInt(num);    
-    
-    if(userTotal===totalRandomNum) // this is a win
-    {
-        wins++;        
-        $("#wins").text("Wins: "+wins);
-        console.log("in wins  : "+wins);
-        startReset();
-    }
-    else if(userTotal > totalRandomNum) // this is a loss
-    {
-        losses++;
-        $("#losses").text("Losses: "+losses);
-        console.log("in losses : "+losses);        
-        startReset();
-    }
-    else
-    {
-        null;
-    }
-}); 
+    imgClick("#redButton");
+});
+
+$("#lavenderButton").click(function(){
+    imgClick("#lavenderButton");
+});
+
 $("#yellowButton").click(function(){
-    
-    num = $("#yellowButton").attr("value");
-    userTotal = userTotal + parseInt(num);    
-    
-    if(userTotal===totalRandomNum) // this is a win
-    {
-        wins++;        
-        $("#wins").text("Wins: "+wins);
-        console.log("in wins  : "+wins);
-        startReset();
-    }
-    else if(userTotal > totalRandomNum) // this is a loss
-    {
-        losses++;
-        $("#losses").text("Losses: "+losses);
-        console.log("in losses : "+losses);        
-        startReset();
-    }
-    else
-    {
-        null;
-    }
-}); 
+    imgClick("#yellowButton");
+});
+
 $("#greenButton").click(function(){
-    
-    num = $("#greenButton").attr("value");
-    userTotal = userTotal + parseInt(num);    
-    
-    if(userTotal===totalRandomNum) // this is a win
-    {
-        wins++;        
-        $("#wins").text("Wins: "+wins);
-        console.log("in wins  : "+wins);
-        startReset();
-    }
-    else if(userTotal > totalRandomNum) // this is a loss
-    {
-        losses++;
-        $("#losses").text("Losses: "+losses);
-        console.log("in losses : "+losses);        
-        startReset();
-    }
-    else
-    {
-        null;
-    }
+    imgClick("#greenButton");
 }); 
